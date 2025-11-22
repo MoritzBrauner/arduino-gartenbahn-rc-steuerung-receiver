03f6288b00d217432698b2c282bb916d5219e22e
Moritz Brauner <brauner.moritz@googlemail.com>
2025-11-22 18:35:07 +0100

    First Blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); 
+  Serial.println("=== Setup started ===");
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("=== Setup finished ===");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  Serial.println("LED ON");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  Serial.println("LED OFF");
+  digitalWrite(LED_BUILTIN, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
